65559d7a8867bf93d50ed0eeb03bf5774a79137a
mcm <joohyuna2@gmail.com>
2026-03-26 12:15:49 +0900

    환경변수 적용

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@ import express from "express";
 import dotenv from "dotenv";
 
 // 1. 환경 변수 초기화
-dotenv.config();
+dotenv.config();  // 프로그램이 실행되자 마자 환경변수를 불러와야 한다. 가장 먼저 준비
 
 //2. Express 앱  생성
-const app = express();
-app.use(express.json());   // app.use 라는 매소드는 미들웨어를 사용하게 할 때 사용
+const app = express();  // 공장을 만드는것의 시작 물류창고라고 생각하자
+app.use(express.json());   // app.use 라는 매소드는 미들웨어를 사용하게 할 때 사용 use (rmsid )
 // 미들웨어 컨베이어 벨트 지나가기 전에 일꾼에게 가기 전에 경비원, 아니면 방사선 같이 json을 사용하게 할꺼야
-
 // 데이터베이스가 없어서 임시로 만든 데이터 변수 (mock)
 const mockPosts = [  // 글 목록을 주는 api
     { id: 1, title: "첫 번째 택배", content: "무사히 도착했습니다." },
@@ -18,7 +17,7 @@ const mockPosts = [  // 글 목록을 주는 api
 ];
 
 //  "/" 로 들어왔을 때 동작되는 일꾼
-// app.get("/", () => {});
+// app.get("/", () => {}); 기본 모양임 이것으로 시작하기
 app.get("/", (req, res) => {
     // res.send 는 string을 내보낼때
     res.send("여기는 루트입니다.");
@@ -33,15 +32,16 @@ app.get("/posts", (req, res) => {
 app.get("/hello", (a, b) => {
     b.send("여기는 hello 주소로 들어왔습니다.");
 });
-app.get("/" , (req, res) => {
-    res.send("<h1>서버가 실행되었습니다.</h1><div>http://localhost:3000</div>");
-});
+
+// app.get("/" , (req, res) => {
+//     res.send("<h1>서버가 실행되었습니다.</h1><div>http://localhost:3000</div>");
+// });
 
 // 3. app.listen : 서버를 실행하는 메소드
 //          매개변수 2개 (포트번호, 함수)
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     // 얘가 실행되면  처음 할일
-    console.log("<h1>서버가 실행되었습니다.</h1><div>http://localhost:3000</div>");
+    console.log(`서버가 실행되었습니다. http://localhost:${process.env.PORT}`);
 });
 
 
