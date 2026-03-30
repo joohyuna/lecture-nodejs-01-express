6457e16d64d0b80ee5ff884bda869218315ca0de
mcm <joohyuna2@gmail.com>
2026-03-30 16:42:35 +0900

    /users/login 기능 생성 userRoutes.ts 수정

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -12,7 +12,7 @@ router.get("/users/login", (req, res) => {
     // const email = req.query.email;
     // const password = req.query.password;
     // req.query는 객에에서 1개이상의 프로퍼티 값을 뽕아로여 한다면
-    const { email, password } = req.query;
+    const { email, password } = req.query;  // 쿼리 스트링으로 들어온값을 꺼내는 방법
     // res.send(`email: ${email}, password: ${password}`);
 
     const successPage = path.join(process.cwd(), "public", "success.html");
@@ -24,6 +24,7 @@ router.get("/users/login", (req, res) => {
     if (!email || !password) {
         return res.sendFile(failPage);
     }
+
     // 2. email로 값을 찾았을때 있는지           -> 성공, 실패
     // 그 중에 들어온 값중 고유값 으로 검색 (id값은 고유값)
     const user = mockUsers.find((value) => {
@@ -32,6 +33,8 @@ router.get("/users/login", (req, res) => {
     if (!user) {
         return res.sendFile(failPage);
     }
+
+
     // 3. 그 찾은 그 외원정보와 비밀번호를 비교해서 맞는지  -> 성공, 실패
     // 더 비교할 값이 있으면 그걸로 일치하는지 확인
     if (user.password !== password) {
