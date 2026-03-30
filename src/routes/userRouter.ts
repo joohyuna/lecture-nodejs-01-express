0f839e3508287330126387a55505b49d4488c02f
mcm <joohyuna2@gmail.com>
2026-03-30 17:52:47 +0900

    userRouter.ts 연습

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,49 +1,39 @@
-import express from "express";
-import path from "path";
+// 1. 모듈 불러오기
+import express from "express";  // 서버 공장을 만들기 위한 라이브러리 Express불러오기
+import path from "path";  // 파일 안전하게 확인할수 있는 Node.js 기본 모둘
 
-const router = express.Router();   // alt + enter express 아무데나 커서 놓고
+// 2. 라우터 생성
+const router = express.Router();  // 주소별 기능을 나누는 공장의 기능별 주소
 
+// 3. 가상 DB 만들기
 const mockUsers = [
-    {id: 1, email: "admin@test.com", password: "password123", name: "관리자"},
+    {id: 1, email: "admin@test.com", password: "password1234", name: "관리자"},
     {id: 2, email: "user@test.com", password: "1234", name: "일반유저"},
 ];
 
+//
 router.get("/users/login", (req, res) => {
-    // const email = req.query.email;
-    // const password = req.query.password;
-    // req.query는 객에에서 1개이상의 프로퍼티 값을 뽕아로여 한다면
-    const { email, password } = req.query;  // 쿼리 스트링으로 들어온값을 꺼내는 방법
-    // res.send(`email: ${email}, password: ${password}`);
+    const {email, password} = req.query;
 
     const successPage = path.join(process.cwd(), "public", "success.html");
     const failPage = path.join(process.cwd(), "public", "fail.html");
 
-    // 0. 로그인 이 있으니 먼저 메일 확인
-    // 1. email과 password가 둘다 도착이 됐는지  -> 성공, 실패
-    // 값이 있는지 없는지 판별
+    //1. email, password 둘다 도착했는지 확인 -> 성공, 실패
     if (!email || !password) {
         return res.sendFile(failPage);
     }
-
-    // 2. email로 값을 찾았을때 있는지           -> 성공, 실패
-    // 그 중에 들어온 값중 고유값 으로 검색 (id값은 고유값)
+    // 2. email로 값을 찾았을 때 있는 지    -> 성공, 실패
     const user = mockUsers.find((value) => {
         return value.email === email;
     });
     if (!user) {
         return res.sendFile(failPage);
     }
-
-
-    // 3. 그 찾은 그 외원정보와 비밀번호를 비교해서 맞는지  -> 성공, 실패
-    // 더 비교할 값이 있으면 그걸로 일치하는지 확인
+    // 3. 찾은 회원정보와 비밀번호를 비교해서 맞는지 -> 성공, 실패
     if (user.password !== password) {
         return res.sendFile(failPage);
     }
-    res.sendFile(successPage);
+    return res.sendFile(successPage);
 });
-// router.get("/users/login", (req, res) => {
-//     res.send("로그인 페이지");
-// });
 
-export default router;
+export default router; //
